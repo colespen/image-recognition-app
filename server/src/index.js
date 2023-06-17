77e148e990e903f568186caa4f67fbed4670209d
Spencer Cole <spencerlewiscole@gmail.com>
2023-06-16 22:52:43 -0400

    add pinecode config

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,23 @@ import * as dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
-import handler from "./handler.js";
+import { handleImage, handleDeleteUser } from "./handlers.js";
 
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 8001;
 
 const app = express();
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
-app.post("/api/image", handler);
+
+app.get("/api/health", async (_, res) => {
+  res.send("ok");
+});
+
+app.post("/api/image", handleImage);
+
+app.delete("/api/user", handleDeleteUser);
 
 // Start the HTTP server
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
