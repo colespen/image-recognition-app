6f1c64fc651c3750acb26cf6ad6ad5a8e4fcbd76
Spencer Cole <spencerlewiscole@gmail.com>
2023-06-16 21:25:07 -0400

    send images to huggingface enpoint for embeddings

--- server/src/handlers.js
+++ server/src/handlers.js
@@ -1,3 +1,4 @@
+// receive images from device
 const handleImage = async (req, res) => {
   const data = req.body;
 
@@ -22,3 +23,28 @@ const handleImage = async (req, res) => {
     res.status(500).json({ message });
   }
 };
+
+// send images with text to Hugging Face enpoint and return embedding
+const getEmbeddings = async (imageBase64, text) => {
+    const data = {
+      inputs: {
+        image: imageBase64,
+        text,
+      },
+    };
+    try {
+      const response = await fetch(inferenceEndpointUrl, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${inferenceEndpointToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const json = await response.json();
+      return json.embeddings;
+    } catch (e) {
+      console.log(e);
+    }
+  };
